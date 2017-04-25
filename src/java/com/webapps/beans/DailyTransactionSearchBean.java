60e220457abbae211356eb6adbd20377f3467d17
reidbraswell1 <reidbras@gmail.com>
2017-04-25 17:17:51 -0500

    Committing new files for WebApplication2

---
+++ src/java/com/webapps/beans/DailyTransactionSearchBean.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webapps.beans;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ *
+ * @author reid
+ */
+public class DailyTransactionSearchBean {
+    
+    private Date date;
+    private BigDecimal amount;
+    private String account;
+    private String note;
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the amount
+     */
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * @return the account
+     */
+    public String getAccount() {
+        return account;
+    }
+
+    /**
+     * @param account the account to set
+     */
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    /**
+     * @return the note
+     */
+    public String getNote() {
+        return note;
+    }
+
+    /**
+     * @param note the note to set
+     */
+    public void setNote(String note) {
+        this.note = note;
+    }
+}//DailyTransactionSearchBean//
