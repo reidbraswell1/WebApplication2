b2245e1ac30cd372dd66f81ff468129d4c74b45d
reidbraswell1 <reidbras@gmail.com>
2017-05-03 10:47:28 -0500

    Added java script function to hide input box.

--- web/includes/js/DailyTransactions.js
+++ web/includes/js/DailyTransactions.js
@@ -9,6 +9,19 @@ function unhideInput() {
        y.setAttribute('type','hidden');
     }
 }//unhideInput//
+function unhideInputText() {
+//alert("hello");
+    var x=document.getElementById("selectNote");
+    var y=document.getElementById("newNote");
+    if(x.value=="Other") {
+       y.setAttribute('type','text');
+       x.style.width="75px";  
+    }
+    else {
+       y.setAttribute('type','hidden');
+       x.style.width="";
+    }
+}//unhideInput//
 function hideInput() {
 //alert("hello");
     var y=document.getElementById("newAccount");
